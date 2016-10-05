23b9ea05912a0dd4b49a3e506989e2ae0f7329a8
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-10-05 10:31:56 -0300

    Add contacts editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title.component';
-import { HighlightDirective } from './highlight.directive';
+import { ContactComponent } from './contact/contact.component';
 
+import { HighlightDirective } from './highlight.directive';
+import { HighlightDirective as ContactHighlightDirective } from './contact/highlight.directive';
+
+import { AwesomePipe } from './contact/awesome.pipe';
+
+import { ContactService } from './contact/contact.service';
 import { UserService } from './user.service';
 
 @NgModule({
-  declarations: [AppComponent, TitleComponent, HighlightDirective],
-  imports: [BrowserModule],
-  providers: [UserService],
+  declarations: [
+    AppComponent,
+    HighlightDirective,
+    TitleComponent,
+
+    AwesomePipe,
+    ContactComponent,
+    ContactHighlightDirective
+  ],
+  imports: [BrowserModule, FormsModule],
+  providers: [ContactService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
