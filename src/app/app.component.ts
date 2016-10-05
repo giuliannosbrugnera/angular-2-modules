497e507560c77b5735675630daf5583ae34b5d4a
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-10-05 09:19:53 -0300

    Create a directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1>',
+  template: '<h1 highlight>{{title}}</h1>'
 })
 export class AppComponent {
   title = 'Minimal NgModule';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive'
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, HighlightDirective],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
