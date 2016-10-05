8674675440dfdd68cb3a3059b9fedd04565ee5ad
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-10-05 09:17:35 -0300

    Minimal example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: '<h1>{{title}}</h1>',
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Minimal NgModule';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
+  imports: [BrowserModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
